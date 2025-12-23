537dbb90f8af05ef15a7434da4561c57b7193c75
Senum2001 <94157820+Senum2001@users.noreply.github.com>
2025-12-23 21:58:30 +0530

    5

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || '',
+  siteUrl: process.env.SITE_URL || 'https://senum.dev',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   robotsTxtOptions: {
